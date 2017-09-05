de40332eef1db9473e79e7b58ae14a2064c50f59
akiori <akilok@hotmail.com>
2017-09-05 23:02:54 +0800

    exclude: if the received message is from someone who speaks in a group

--- message_fowardTo_.ts
+++ message_fowardTo_.ts
@@ -1,4 +1,4 @@
-const { Wechaty, log, Message, Contact } = require('wechaty')
+const { Wechaty, log, Message, Contact, MediaMessage } = require('wechaty')
 const qrcode = require('qrcode-terminal')
 
 Wechaty.instance()
@@ -14,14 +14,21 @@ Wechaty.instance()
             const inMessage = m.from()
             // From whose meassage?
             // MessageInstance.name()
-            if (inMessage.name() === 'm.a.u.d') {
-                // fetch the content of message
-                const cont = m.content()
-                // To whom I will forward to?
-                // Contact.find(Query)
-                const forwardTo = await Contact.find({ name: '陆俊华的大脑管理员' })
-                
-                await forwardTo.say(cont)
+            if (m.room() == null) {
+                if (inMessage.name() === '巫英才' || inMessage.name() === 'm.a.u.d') {
+                    let cont = "";
+                    if (m instanceof MediaMessage) {
+                        cont = "发的不是文字"
+                    }
+                    else {
+                        // fetch the content of message
+                        cont = m.content()
+                    }
+                    // To whom I will forward to?
+                    // Contact.find(Query)
+                    const forwardTo = await Contact.find({ name: '陆俊华的大脑管理员' })
+                    await forwardTo.say(inMessage.name() + " said: " + cont)
+                }
             }
         }
 
